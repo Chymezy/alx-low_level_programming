5980db7250ffc5ccd31829ab347a945710c946a3
Chymezy <chymezy332@gmail.com>
2023-08-21 14:22:43 -0700

    adding files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer and updates it to 98
+ *
+ * @n: parameter storing the pointer value
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

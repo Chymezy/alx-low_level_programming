9e07f74ea665455603c3186cf8b98df445f6f999
Chymezy <chymezy332@gmail.com>
2023-08-24 12:58:18 -0700

    adding 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncat - combines two string according to number of character
+ * @dest: destination string to be returned
+ * @src: source string to be combined
+ * @n: number of string to be combined
+ *
+ * Return: string on success
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *save_dest = dest;
+	int len_dest = strlen(dest);
+	int i = 0;
+
+	while (*dest)
+		dest++;
+
+	for (; src[i] != '\0' && i < n; i++)
+	{
+		dest[len_dest + i] = src[i];
+	}
+	*dest = '\0';
+	return (save_dest);
+}

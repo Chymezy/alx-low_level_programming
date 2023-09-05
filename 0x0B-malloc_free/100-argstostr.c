6bdf0cefe2855323baa9a96e4e94b0946d242759
Chymezy <chymezy332@gmail.com>
2023-09-05 16:34:15 -0700

    adding files

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * argstostr - combines all arugments passed with program
+ * @ac: number of arugments
+ * @av: array of arguments
+ *
+ * Return: Pointer to a new string or NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, total = 0, count = 0;
+	char *s;
+
+	if (ac == 0 || av == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0';)
+			j++;
+		total += j;
+	}
+	s = (char *)malloc((total + 1));
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		s[count] = '\n';
+		for (j = 0; av[i][j] != '\0'; count++, j++)
+		{
+			s[count] = av[i][j];
+		}
+		s[count] = '\n';
+		count++;
+
+	}
+	return (s);
+
+}

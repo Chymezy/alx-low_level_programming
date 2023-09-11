f69d53e4ee6f752ec3bce46eff89e52f7c7fb5a8
Chymezy <chymezy332@gmail.com>
2023-09-11 15:02:50 -0700

    adding file 1 with new comment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure dog
+ * @name: name pararmeter
+ * @age: age pararmeter
+ * @owner: owner pararmeter
+ */
 struct dog
 {
 	char *name;

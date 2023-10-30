df0f47e13a4aff7f900b00c8b9d6837d2c1d9d17
Chymezy <chymezy332@gmail.com>
2023-10-30 14:43:36 -0700

    adding files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints.
+ * @filename: File pointer.
+ * @letters: The number of letters to print
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wt;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(o, buffer, letters);
+	wt = write(STDOUT_FILENO, buffer, r);
+
+	if (op == -1 || rd == -1 || wt == -1 || wt != rd)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
